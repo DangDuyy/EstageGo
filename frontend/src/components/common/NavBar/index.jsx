88a8940e4da8003687abdb1a3408ebe66d974517
DangDuyy <duyprovn987@gmail.com>
2025-09-01 10:03:46 +0700

    refactor avatar user when usenSlice not null

--- frontend/src/components/common/NavBar/index.jsx
+++ frontend/src/components/common/NavBar/index.jsx
@@ -6,9 +6,14 @@ import ToogleMode from "./toggle-mode";
 import { Folders, User } from "lucide-react";
 import { useState } from "react";
 import LoginModal from "../Modal/login";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "@/redux/user/userSlice";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const NavBar = () => {
   const [openModal, setOpenModal] = useState(false)
+
+  const currentUser = useSelector(selectCurrentUser)
 
   return (
     <div className="min-h-[95px] bg-muted">
@@ -22,13 +27,18 @@ const NavBar = () => {
 
           <div className="flex items-center gap-6">
             <ToogleMode />
-            <Button 
-              variant="outline" 
-              className="hidden sm:inline-flex rounded-full text-lg px-7 py-3 h-14 min-w-[120px] cursor-pointer" 
-              onClick={() => setOpenModal(true)}>
-              <User />
-              Sign In
-            </Button>
+            { currentUser 
+              ? <Avatar className="h-13 w-13">
+                  <AvatarImage src={currentUser.avatar} />
+                  <AvatarFallback>{currentUser.name}</AvatarFallback>
+                </Avatar>
+              : <Button 
+                variant="outline" 
+                className="hidden sm:inline-flex rounded-full text-lg px-7 py-3 h-14 min-w-[120px] cursor-pointer" 
+                onClick={() => setOpenModal(true)}>
+                <User />
+                Sign In
+              </Button> }
             <Button className="rounded-full text-lg px-7 py-3 h-14 min-w-[200px] cursor-pointer">
               <Folders />
               Submit Property
