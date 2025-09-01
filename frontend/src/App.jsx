c806818bc7b2a0a8b9c14f97b54ad286225d3dca
DangDuyy <duyprovn987@gmail.com>
2025-09-01 14:26:13 +0700

    apply sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/404'
 import PropertyPage from './pages/PropertyPage'
+import { Sidebar } from './components/common/SidebarMenu/sidebar'
 function App() {
   return (
     <Routes>
@@ -12,6 +13,8 @@ function App() {
 
       <Route path="/home" element={<HomePage/>} />
       <Route path="/properties" element={<PropertyPage/>} />
+      <Route path="/dashboard" element={<Sidebar/>} />
+
       {/* page not found */}
       <Route path="*" element={<NotFoundPage/>} />
     </Routes>
---
+++ frontend/src/pages/DashboardPage/index.jsx
@@ -0,0 +1,10 @@
+import { Sidebar } from '@/components/common/SidebarMenu/sidebar'
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <Sidebar />
+  )
+}
+
+export default DashboardPage
