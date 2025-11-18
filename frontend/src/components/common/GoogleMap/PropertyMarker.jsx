a324aef235f927789fede8061e18c29207bc512c
vovan <vovantuan7702@gmail.com>
2025-11-18 12:55:33 +0700

    fix bug property card không tương tác được trên bản đồ

--- frontend/src/components/common/GoogleMap/PropertyMarker.jsx
+++ frontend/src/components/common/GoogleMap/PropertyMarker.jsx
@@ -6,36 +6,32 @@ import PropertyCard from "../Property/FeatureCard/PropertyCard";
 function PropertyMarker({ property }) {
     const position = { lat: property.address?.location?.coordinates[1], lng: property.address?.location?.coordinates[0] }
     return (
-        <OverlayView key={property._id} position={position} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <div
-                        className="
-                          w-3.5 h-3.5
-                          bg-red-700
-                          hover:bg-green-700
-                          rounded-full
-                          border-2 border-white
-                          shadow-xl
-                          -translate-x-1/2 -translate-y-1/2
-                        "
-                        style={{
-                            boxShadow: "0 4px 6px -2px rgba(0, 0, 0, 0.9)"
-                        }}
-                        onClick={() => { console.log("aslgkja;shg;asjdg;lajsgl;jasl;dgjasldgj") }}
+        <OverlayView
+            key={property._id}
+            position={position}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        >
+            <div className="relative pointer-events-auto">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <div
+                            className="
+                        w-3.5 h-3.5 bg-red-700 rounded-full border-2 border-white
+                        shadow-xl -translate-x-1/2 -translate-y-1/2
+                        hover:bg-green-700
+                    "
+                        />
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent
+                        className="m-0 p-0 border-0"
                     >
-
-                    </div>
-                </DropdownMenuTrigger>
-
-                <DropdownMenuContent className={'m-0 p-0 border-0'} asChild>
-                    <PropertyCard
-                        key={property._id}
-                        item={property}
-                    />
-                </DropdownMenuContent>
-            </DropdownMenu>
+                        <PropertyCard item={property} />
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </OverlayView>
+
     );
 }
 
